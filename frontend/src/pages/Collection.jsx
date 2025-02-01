c2723442f4b12266dc3138b95b2158e9bc576488
fouad mesrane <fouadmesrane@gmail.com>
2025-02-01 08:48:47 -0700

    sort function to sort products

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -10,7 +10,9 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType,setSortType] = useState('relevent');
 
+  // category
   const toggleCategory = (e) => {
     if (category.includes(e.target.value)) {
       setCategory((prev) => prev.filter((item) => item !== e.target.value));
@@ -18,6 +20,7 @@ const Collection = () => {
       setCategory((prev) => [...prev, e.target.value]);
     }
   };
+  // sub category 
   const toggleSubCategory = (e) => {
     if (subCategory.includes(e.target.value)) {
       setSubCategory((prev) => prev.filter((item) => item !== e.target.value));
@@ -26,26 +29,49 @@ const Collection = () => {
     }
   };
 
-
-  // 
+  // filter function
   const applyFilter = () => {
-    let productsCopy = products.slice()
-    if(category.length > 0) {
-      productsCopy = productsCopy.filter(item => category.includes(item.category))
+    let productsCopy = products.slice();
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        category.includes(item.category)
+      );
     }
-    if(subCategory.length > 0) {
-      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory))
+    if (subCategory.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        subCategory.includes(item.subCategory)
+      );
     }
 
-    setFilterProducts (productsCopy)
-  }
+    setFilterProducts(productsCopy);
+  };
 
- 
+  // sort function
+  const sortProduct = () => {
+    let filterProductCopy = filterProducts.slice();
+    switch (sortType) {
+      case "low-high":
+        setFilterProducts(filterProductCopy.sort((a, b) => a.price - b.price));
+        break;
+      case "high-low":
+        setFilterProducts(filterProductCopy.sort((a, b) => b.price - a.price));
+        break;
+      default:
+        applyFilter();
+        break;
+    }
+  };
+  
+// initial filter
+  useEffect(() => {
+    applyFilter();
+  }, [category, subCategory]);
 
-  useEffect(()=> {
-    applyFilter()
-  },[category, subCategory])
- 
+  // sort
+  useEffect(() => {
+    sortProduct()
+  },[sortType])
+
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
       {/* filter options */}
@@ -155,7 +181,7 @@ const Collection = () => {
         <div className="flex justify-between text-base sm:text-2xl mb-4">
           <Title text1={"ALL"} text2={"COLLECTIONS"} />
           {/* product sort */}
-          <select className="border border-gray-300 text-sm px-2">
+          <select onChange={(e) => setSortType(e.target.value)} className="border border-gray-300 text-sm px-2">
             <option value="relavent">Sort by : Relevent</option>
             <option value="low-high">Sort by : Low to High</option>
             <option value="high-low">Sort by : High to Low</option>
