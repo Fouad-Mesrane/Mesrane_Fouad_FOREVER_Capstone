225ff6ef676016c011f9bb89b2b96254f6bb5c73
fouad mesrane <fouadmesrane@gmail.com>
2025-02-03 09:58:46 -0700

    finished collection page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,23 +1,26 @@
 import { createContext } from "react";
 import { products } from "../assets/frontend_assets/assets";
-export const ShopContext = createContext()
-
+import { useState } from "react";
+export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
+  const currency = "$";
+  const deliveryFee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const value = {
+    products,
+    currency,
+    deliveryFee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
 
-    const currency = '$'
-    const deliveryFee = 10
-    const value = {
-        products,
-        currency,
-        deliveryFee
-    }
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
-    return (
-        <ShopContext.Provider value={value}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
-
-export default ShopContextProvider
+export default ShopContextProvider;
