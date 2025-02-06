37adf4931a93e5edd440cead0f501ae7e0ef10e4
fouad mesrane <fouadmesrane@gmail.com>
2025-02-06 08:19:49 -0700

    started backend and installed needed dependencies

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-col md:flex-row gap-10 justify-center mb-28">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.contact_img}
+          alt=""
+        />
+        <div className="flex flex-col gap-6  justify-center items-start">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            1111 Quebec St <br />
+            New York, NY 10012
+          </p>
+          <p className="text-gray-500">
+            Tel: +1-221-34-2345 <br />
+            Email: contact@foreveryou.com
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Forever
+          </p>
+          <p className="text-gray-500">Learn more about careers at Forever.</p>
+          <button className=" border border-black hover:text-white text-sm px-8 py-4 hover:bg-black transition-all ease-in duration-300">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+
+      <NewsletterBox />
+    </div>
+  );
+};
+
+export default Contact;
