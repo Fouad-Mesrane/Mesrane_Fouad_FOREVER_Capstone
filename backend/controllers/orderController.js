d2b83744fdbfdb83798cdd3d531172bbcbc83b1b
fouad mesrane <fouadmesrane@gmail.com>
2025-02-15 16:01:14 -0700

    added api endpoints and placeOrder functionality for COD

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,1 +1,52 @@
-import Order from "../models/Order";
+import Order from "../models/Order.js";
+import User from "../models/User.js";
+
+// Placing an order using COD
+
+const placeOrder = async (req, res) => {
+    try {
+        const { userId, products, amount, address } = req.body;
+        const order = new Order({
+            userId,
+            products,
+            amount,
+            address,
+            paymentMethod : "COD",
+            payment : false
+        });
+
+        const newOrder = await order.save();
+        await User.findByIdAndUpdate(userId, {cartData: {}});
+
+        res.status(200).json({ success: true, message: "Order placed successfully", order: newOrder });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+};
+
+// Placing an order using Stripe
+const placeOrderStripe = async (req, res) => {};
+
+// Placing an order using Razorpay
+const placeOrderRazorpay = async (req, res) => {};
+
+// Get all orders for admin panel
+
+const allOrders = async (req, res) => {};
+
+// User orders for frontend
+const userOrders = async (req, res) => {};
+
+// update order status from admin panel
+
+const updateOrderStatus = async (req, res) => {};
+
+export {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateOrderStatus,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import  "dotenv/config";
 import connectDB from "./config/mongodb.js";
@@ -7,6 +6,7 @@ import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import cartRouter from "./routes/cartRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 // App config
 const app = express();
 const port = process.env.PORT || 9000;
@@ -22,7 +22,7 @@ app.use(cors());
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
-
+app.use('/api/order', orderRouter)
 
 
 app.get("/", (req, res) => {
